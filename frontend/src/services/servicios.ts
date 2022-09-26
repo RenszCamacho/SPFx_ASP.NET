5c7648aa4d2359d75a275a3376952dbd2730c59a
Renszo D'Giovanni <renszo.camacho@raona.com>
2022-09-26 11:28:32 +0200

    Reconfigurar los servicios

--- frontend/src/services/servicios.ts
+++ frontend/src/services/servicios.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   HttpClient,
   IHttpClientOptions,
@@ -7,6 +8,8 @@ import { WebPartContext } from "@microsoft/sp-webpart-base";
 import { IItem } from "../interface/IItem";
 
 export class ServicesProvider {
+  private _URL = "https://localhost:7253/api";
+
   private _wpContext: WebPartContext;
 
   public constructor(context: WebPartContext) {
@@ -16,18 +19,62 @@ export class ServicesProvider {
   private _httpClientOptionsForGlobal: IHttpClientOptions = {
     headers: new Headers(),
     method: "GET",
-    // mode: "cors",
+    mode: "no-cors",
   };
 
   public async getItems(): Promise<IItem[]> {
     try {
       const response: HttpClientResponse = await this._wpContext.httpClient.get(
-        "https://localhost:7253/api/contactos",
+        this._URL + "/contactos",
         HttpClient.configurations.v1,
-        this._httpClientOptionsForGlobal
+        {
+          headers: new Headers(),
+          method: "GET",
+        }
       );
       const responseJson: IItem[] = await response.json();
       return responseJson;
+    } catch (error) {
+      throw new Error("La llamada httPClient tuvo un error" + error);
+    }
+  }
+
+  public async postItem(payload: IItem): Promise<IItem> {
+    const requestHeaders: Headers = new Headers();
+    requestHeaders.append("Accept", "*/*");
+    requestHeaders.append("Content-type", "application/json");
+    requestHeaders.append("Cache-Control", "no-cache");
+
+    let httpClientOptions: IHttpClientOptions = {
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      },
+      mode: "no-cors",
+      method: "POST",
+    };
+
+    if (payload) {
+      httpClientOptions = {
+        ...httpClientOptions,
+        body: JSON.stringify(payload),
+      };
+    }
+
+    try {
+      const response: HttpClientResponse =
+        await this._wpContext.httpClient.post(
+          "https://localhost:7253/api/contactos",
+          HttpClient.configurations.v1,
+          httpClientOptions
+        );
+
+      const responseJSON = await response.json();
+      return responseJSON;
+
+      // const responseJson: IItem = await response.json();
+      // return responseJson;
     } catch (error) {
       throw new Error("La llamada httPClient tuvo un error" + error);
     }
